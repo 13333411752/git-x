735a74cec84548d45b9b14d5ab24aca714b5138a
lenovo <1373293083@qq.com>
2023-03-11 17:39:16 +0800

    666

---
+++ src/main/java/com/java/Hello.java
@@ -0,0 +1,13 @@
+package com.java;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Create by 2023/3/11 17:38
+ * @Description
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
